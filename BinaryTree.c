f7613c0fd130dea3dd37127481c2eefe486c5ad9
stormaroon <stormaroon@gmail.com>
2016-11-25 16:59:13 +0800

    添加最大最小值查找函数

--- BinaryTree.c
+++ BinaryTree.c
@@ -9,11 +9,18 @@ typedef struct BNode
     ElemType data;
     struct BNode *left, *right;
 } BNode, *BTree;
-typedef BNode* Position;
+typedef BNode *Position;
 
+// 根据队列给定的数据创建树 (前序遍历)
 void createTree(BTree *t);
+// 前序遍历树
 void preOrder(BTree T);
+// 根据给定的元素查找， 返回找到的位置的地址
 Position find(ElemType e, BTree T);
+// 查找树中最小的元素
+Position findMin(BTree T);
+// 查找树中最大的元素
+Position findMax(BTree T);
 
 int main(int argc, char const *argv[])
 {
@@ -24,7 +31,12 @@ int main(int argc, char const *argv[])
     // 因为树的内容尚未初始化，所以传指针
     createTree(&T);
     preOrder(T);
-    printf("%d\n", find(50, T) -> data);
+    printf("test find: %d\n", find(50, T) -> data);
+    printf("test findMin: %d\n", findMin(T) -> data);
+    printf("test findMax: %d\n", findMax(T) -> data);
+
+    printf("\n");
+    printf("press any key to exit~~~");
     getchar();
     return 0;
 }
@@ -62,7 +74,7 @@ void preOrder(BTree T)
 }
 
 Position find(ElemType e, BTree T)
-{   // 递归版本
+{ // 递归版本
     //if (T == NULL) return NULL;
     //if (e > T -> data) return find(e, T -> right);
     //if (e < T -> data) return find(e, T -> left);
@@ -70,10 +82,33 @@ Position find(ElemType e, BTree T)
     // 递归版本结束
 
     // 非递归版本
-    while (T) {
-        if (e > T -> data) T = T -> right;
-        if (e < T -> data) T = T -> left;
-        if (e == T -> data) return T;
+    while (T)
+    {
+        if (e > T->data)
+            T = T->right;
+        if (e < T->data)
+            T = T->left;
+        if (e == T->data)
+            return T;
     }
     return NULL;
+}
+
+Position findMin(BTree T)
+{
+    if (T == NULL)
+        return NULL;
+    else if (T->left)
+        findMin(T->left);
+    else
+        return T;
+}
+
+Position findMax(BTree T)
+{
+    if (T)
+    {
+        while (T -> right) T = T->right;
+    }
+    return T;
 }
